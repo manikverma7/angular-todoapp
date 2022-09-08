40b8766b27dee6b80cb3d0c6ce3a8734b67b3a9c
manikverma7 <manikverma6803@gmail.com>
2022-09-09 02:13:05 +0530

    Todo list complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
+
+export const fadeAnimation = trigger('fadeInGrow', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('300ms', style({ opacity: 1 })),
+  ]),
+  transition(':leave', [
+    style({ opacity: 1 }),
+    animate('300ms', style({ opacity: 0 })),
+  ]),
+]);
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        stagger('60ms', animate('600ms ease-out', style({ opacity: 1 }))),
+      ],
+      { optional: true }
+    ),
+    query(':leave', animate('500ms', style({ opacity: 0 })), {
+      optional: true,
+    }),
+  ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation, listAnimation],
 })
 export class AppComponent {
-  title = 'angular-todoapp';
+  title: string = "Manik's Todo List";
+
+  todoValue: string = '';
+  list: Todo[] = [];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
